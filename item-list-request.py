f2bb63bf5f0fbf14200eb2b69599214088c079d4
masonsnider30@gmail.com <masonsnider30@gmail.com>
2018-08-30 14:39:20 +0000

    Added the folder that comes from the default Python library in Cloud9

--- item-list-request.py
+++ item-list-request.py
@@ -1,6 +1,15 @@
-#import the request library
+#imports
 import requests
+import json
 
-#api-endpoint
+
 URL = "https://api.warframe.market/v1/items"
 
+
+client = requests.session()
+
+
+item_list = json.loads(client.get(URL).text)['payload']['items']['en']
+
+
+print json.dumps(item_list, indent=2, sort_keys=False)
