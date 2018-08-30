be1e4962e578bc75df3f5bb22ead5d5d8934c109
masonsnider30@gmail.com <masonsnider30@gmail.com>
2018-08-30 17:35:22 +0000

    almost have the item list outputting to an html table. The string outputs to the command line, but not to a text file or html file

--- item-list-to-table.py
+++ item-list-to-table.py
@@ -8,18 +8,28 @@ client = requests.session()
 
 
 item_list = json.loads(client.get(URL).text)['payload']['items']['en']
-html_string = ""
+html_string = '<html><body><table style="width:100%", border=1><tr><th>URL NAME</th><th>ITEM NAME</th><th>ID</th></tr>'
 
 
+i=0
+while i < len(item_list):
+    html_string += "\n<tr><th>" + item_list[i]["url_name"] + "</th><th>" + item_list[i]["item_name"] + "</th><th>" + item_list[i]["id"] + "</th></tr>"
+    i += 1
+    #print(i)
 
-
-
+html_string += "</table></body></html>"
 
 #Print tests
 #print(item_list[0]["item_name"])
 #print(item_list[0]["url_name"])
 #print(item_list[0]["id"])
 
-item_list_htmlfile = open("item-list.html","w")
-item_list_htmlfile.write(html_string)
-item_list_htmlfile.close()
+#item_list_htmlfile = open("item-list.html","w")
+#item_list_htmlfile.write(html_string)
+#item_list_htmlfile.close()
+
+#item_list_textfile = open("item-list-html.txt","w")
+#item_list_textfile.write(html_string)
+#item_list_textfile.close()
+
+print(html_string)
