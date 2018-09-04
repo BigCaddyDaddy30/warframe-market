11bd0de98a7d5906377a585101106029f796974e
masonsnider30@gmail.com <masonsnider30@gmail.com>
2018-09-04 14:21:20 +0000

    this is it!

--- main.py
+++ main.py
@@ -23,24 +23,21 @@ item_list_jsonfile.write(json.dumps(item_list, indent=2, sort_keys=True))
 item_list_jsonfile.close()
 
 
-
-
 #Start grabbing all items
 i=0
 while i < len(item_list):
     URL = "https://api.warframe.market/v1/items/" + item_list[i]['url_name'] + "/orders"
     order_list = json.loads(client.get(URL).text)['payload']['orders']
-    csv_filename = 'master_list.csv'
+    csv_filename = 'master_output.csv'
     #csv_filename = './CSV Files/' + item_list[i]['url_name'] + '.csv'
     master_csv = open(csv_filename, 'a')
     csvwriter = csv.writer(master_csv)
     
+    if i == 0:
+        csvwriter.writerow(["Quantity", "Creation Date", "Order Type", "Platform", "Platinum", "Region", "Last Update", "id", "Item Name"])
+
     j=0
     while j < len(order_list):
-        if j == 0:
-            #header = order_list[j].keys()
-            #csvwriter.writerow(header)
-            csvwriter.writerow(["Quantity", "Creation Date", "Order Type", "Platform", "Platinum", "Region", "Last Update", "id", "Item Name"])
         csvwriter.writerow([order_list[j]['quantity'], order_list[j]['creation_date'], order_list[j]['order_type'], order_list[j]['platform'], order_list[j]['platinum'], order_list[j]['region'], order_list[j]['last_update'], order_list[j]['id'], item_list[i]['item_name']])
         j += 1 
     i += 1
